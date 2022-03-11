daa752f87257e44de4e72a5e2bae04e5bfbffe74
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-11 12:45:42 +0100

    exercise 2 complete

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,13 +1,15 @@
-// Crea un array vuoto.Chiedi per 6 volte all’utente di inserire un numero
-// se è dispari inseriscilo nell’array.
-let arrayDispari = [];
+//Il software deve chiedere per 5 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.Esegui questo programma in due versioni, con il for e con il while.
+let array = [];
 let i = 0;
-for (i = 0; i < 6; i++) {
+let somma = 0;
+
+for (i = 0; i < 5; i++) {
     let numero = parseInt(prompt("inserisci un numero"));
-    if (numero % 2 !== 0) {
-        arrayDispari.push(numero);
-    }
+    array.push(numero);
 }
-for (i = 0; i < arrayDispari.length; i++) {
-    console.log(i + 1 + " numero dispari: " + arrayDispari[i]);
+for (i = 0; i < array.length; i++) {
+    somma += array[i];
 }
+
+console.log("la somma è:", somma);
